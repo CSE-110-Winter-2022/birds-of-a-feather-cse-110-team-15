c6c8285960038290d2ea53046c24671b06e382cc
ishmasashi <77167591+ishmasashi@users.noreply.github.com>
2022-02-01 16:59:33 -0800

    feature to show courses entered (not editable)

--- app/src/main/java/com/example/birdsofafeather/EnterClassActivity.java
+++ app/src/main/java/com/example/birdsofafeather/EnterClassActivity.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class EnterClassActivity extends AppCompatActivity {
     Spinner quarterSpinner, yearSpinner;
+    ArrayList<String> enteredCourseList=new ArrayList<String>();
+    ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_class);
+        ListView courseListView = (ListView) findViewById(R.id.entered_class_list_view);
+
 
         // dropdown for quarter
         quarterSpinner = (Spinner) findViewById(R.id.quarter_spinner);
@@ -29,6 +36,10 @@ public class EnterClassActivity extends AppCompatActivity {
                 R.array.year_array, android.R.layout.simple_spinner_item);
         yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         yearSpinner.setAdapter(yearAdapter);
+
+        // list of courses entered
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, enteredCourseList);
+        courseListView.setAdapter(listAdapter);
     }
 
 
@@ -40,6 +51,18 @@ public class EnterClassActivity extends AppCompatActivity {
         String courseNumber = numberView.getText().toString();
         String courseQuarter = (String) quarterSpinner.getSelectedItem();
         String courseYear = (String) yearSpinner.getSelectedItem();
+
+        if (courseSubject.isEmpty() || courseNumber.isEmpty() ||
+        courseQuarter.isEmpty() || courseYear.isEmpty()){
+            // TODO
+            return;
+        }
+
+
+        String courseEntry = courseSubject + " " + courseNumber + " " + courseQuarter + " "+ courseYear;
+
+        enteredCourseList.add(courseEntry);
+        listAdapter.notifyDataSetChanged();
     }
 
     public void onFinishClicked(View view) {
