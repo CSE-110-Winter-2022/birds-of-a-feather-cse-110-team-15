847cbc07ae36227304c4dd9177a031ead422f106
Nachelle Matute <nachellematute@Nachelles-MBP.attlocal.net>
2022-02-07 02:16:08 -0800

    make code look nice

--- app/src/main/java/com/example/birdsofafeather/UserNameActivity.java
+++ app/src/main/java/com/example/birdsofafeather/UserNameActivity.java
@@ -1,18 +1,9 @@
 package com.example.birdsofafeather;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import com.example.birdsofafeather.models.db.AppDatabase;
-import com.example.birdsofafeather.models.db.StudentWithCourses;
 
 public class UserNameActivity extends AppCompatActivity{
     @Override
@@ -27,7 +18,6 @@ public class UserNameActivity extends AppCompatActivity{
 
     @Override
     protected void onDestroy() {
-        //  saveProfile();
         super.onDestroy();
     }
 
