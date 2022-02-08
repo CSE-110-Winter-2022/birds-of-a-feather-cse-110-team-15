9edbaa3b7db0e71d1b5f14b0248684032c334f58
Chenyu Zhang <chz042@ucsd.edu>
2022-02-07 23:36:05 -0800

    add if statement in onSaveClick and onContinueClick, just in case if the user forgets to paste the URL

--- app/src/main/java/com/example/birdsofafeather/InputHeadshotActivity.java
+++ app/src/main/java/com/example/birdsofafeather/InputHeadshotActivity.java
@@ -34,8 +34,15 @@ public class InputHeadshotActivity extends AppCompatActivity{
     public void onSaveClick(View view) {
         profile = findViewById(R.id.profile);
         String URL = editURL.getText().toString();
-        Picasso.get().load(URL)
-                .into(profile);
+        //if the user pressed save without pasting any URL
+        String defaultURL = "https://lh3.googleusercontent.com/pw/AM-JKLUTkMaSnWQDXiRUw7FdrFk7luoo6VSJqafn8K1Bh1QksFJiO1oOjV5EoUbWnHc7xKtxDGeD9l8R6a7xtdfMFu4iz2y6QovxF0n4e3hZNGcq1izg_XLtUlX-BStPmG1FnGj9VW0wwoOy5G-i4VaNPA9I=s800-no?authuser=0";
+        if(URL.length() == 0) {
+            Picasso.get().load(defaultURL)
+                    .into(profile);
+        }else {
+            Picasso.get().load(URL)
+                    .into(profile);
+        }
     }
 
 
@@ -43,6 +50,13 @@ public class InputHeadshotActivity extends AppCompatActivity{
         //save student to database
         editURL = findViewById(R.id.editURL);
         String URL = editURL.getText().toString();
+        //if the user pressed continue without pasting any URL
+        String defaultURL = "https://lh3.googleusercontent.com/pw/AM-JKLUTkMaSnWQDXiRUw7FdrFk7luoo6VSJqafn8K1Bh1QksFJiO1oOjV5EoUbWnHc7xKtxDGeD9l8R6a7xtdfMFu4iz2y6QovxF0n4e3hZNGcq1izg_XLtUlX-BStPmG1FnGj9VW0wwoOy5G-i4VaNPA9I=s800-no?authuser=0";
+        if(URL.length() == 0) {
+            Picasso.get().load(defaultURL)
+                    .into(profile);
+        }
+        //write the student to database
         AppDatabase db = AppDatabase.singleton(getApplicationContext());
         Student s1 = new Student(db.studentWithCoursesDao().count()+1, name, URL);
         db.studentWithCoursesDao().add(s1);
