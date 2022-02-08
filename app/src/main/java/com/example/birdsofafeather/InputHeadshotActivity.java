84f6b68a3eb730c34b6e2df0f557eca70e36ce6c
Justin Huang <jhuang707@gmail.com>
2022-02-08 00:28:22 -0800

    Display default profile image onCreate, add alerts
    Clean up code with more private variables, add alerts for invalid URLs and trying to continue without entering a URL

--- app/src/main/java/com/example/birdsofafeather/InputHeadshotActivity.java
+++ app/src/main/java/com/example/birdsofafeather/InputHeadshotActivity.java
@@ -16,53 +16,65 @@ public class InputHeadshotActivity extends AppCompatActivity{
     EditText editURL;
     ImageView profile;
     private String name;
+    private boolean hasAlerted;
+    //if the user pressed save without pasting any URL
+    private String defaultURL = "https://lh3.googleusercontent.com/pw/AM-JKLUTkMaSnWQDXiRUw7FdrFk7luoo6VSJqafn8K1Bh1QksFJiO1oOjV5EoUbWnHc7xKtxDGeD9l8R6a7xtdfMFu4iz2y6QovxF0n4e3hZNGcq1izg_XLtUlX-BStPmG1FnGj9VW0wwoOy5G-i4VaNPA9I=s800-no?authuser=0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_headshot);
 
+        // find place to set image
+        profile = findViewById(R.id.profile);
         //find the URL
         editURL = findViewById(R.id.editURL);
+        // show default profile image
+        showHeadshot(defaultURL);
 
         //get the intent
         Bundle extras = getIntent().getExtras();
         name = extras.getString("student_name");
     }
 
+    public void showHeadshot(String URL) {
+        // TODO: need to find better way to check valid URL before displaying
+        try {
+            Picasso.get().load(URL).into(profile);
+        } catch (Exception e) {
+            Picasso.get().load(defaultURL).into(profile);
+            Utilities.showAlert(this, "Invalid URL");
+        }
+    }
+
     //https://guides.codepath.com/android/Displaying-Images-with-the-Picasso-Library
     public void onSaveClick(View view) {
-        profile = findViewById(R.id.profile);
         String URL = editURL.getText().toString();
-        //if the user pressed save without pasting any URL
-        String defaultURL = "https://lh3.googleusercontent.com/pw/AM-JKLUTkMaSnWQDXiRUw7FdrFk7luoo6VSJqafn8K1Bh1QksFJiO1oOjV5EoUbWnHc7xKtxDGeD9l8R6a7xtdfMFu4iz2y6QovxF0n4e3hZNGcq1izg_XLtUlX-BStPmG1FnGj9VW0wwoOy5G-i4VaNPA9I=s800-no?authuser=0";
-        if(URL.length() == 0) {
-            Picasso.get().load(defaultURL)
-                    .into(profile);
-        }else {
-            Picasso.get().load(URL)
-                    .into(profile);
-        }
+        showHeadshot(URL);
     }
 
 
     public void onContinueClick(View view) {
         //save student to database
         editURL = findViewById(R.id.editURL);
-        String URL = editURL.getText().toString();
-        //if the user pressed continue without pasting any URL
-        String defaultURL = "https://lh3.googleusercontent.com/pw/AM-JKLUTkMaSnWQDXiRUw7FdrFk7luoo6VSJqafn8K1Bh1QksFJiO1oOjV5EoUbWnHc7xKtxDGeD9l8R6a7xtdfMFu4iz2y6QovxF0n4e3hZNGcq1izg_XLtUlX-BStPmG1FnGj9VW0wwoOy5G-i4VaNPA9I=s800-no?authuser=0";
-        if(URL.length() == 0) {
-            Picasso.get().load(defaultURL)
-                    .into(profile);
+        String enteredURL = editURL.getText().toString();
+        // alert them once to warn them default profile will be used if no URL entered
+        if(enteredURL.length() == 0 && !hasAlerted) {
+            Utilities.showAlert(this, "No headshot provided, will use default photo as shown. If that is okay, press continue again.");
+            hasAlerted = !hasAlerted;
+            return;
         }
+        String URL = enteredURL.length() == 0 ? defaultURL : enteredURL;
         //write the student to database
         AppDatabase db = AppDatabase.singleton(getApplicationContext());
-        Student s1 = new Student(db.studentWithCoursesDao().count()+1, name, URL);
+        int student_id = db.studentWithCoursesDao().count()+1;
+        Student s1 = new Student(student_id, name, URL);
         db.studentWithCoursesDao().add(s1);
 
         //get the intent
         Intent intent = new Intent(this, EnterCourseActivity.class);
+        // pass newly created student_id
+        intent.putExtra("student_id", student_id);
         startActivity(intent);
         finish();
     }
