6e11da1227137f1c3cae6361f0409f4990dda665
Nachelle Matute <nachellematute@Nachelles-MBP.attlocal.net>
2022-02-12 15:12:18 -0800

    no name entered case

--- app/src/main/java/com/example/birdsofafeather/UserNameActivity.java
+++ app/src/main/java/com/example/birdsofafeather/UserNameActivity.java
@@ -26,10 +26,17 @@ public class UserNameActivity extends AppCompatActivity{
         TextView confirmName = findViewById(R.id.input_name_textview);
         String getConfirmName = confirmName.getText().toString();
 
-        //show name
-        TextView showConfirmName = (TextView) findViewById(R.id.name_view);
-        showConfirmName.setVisibility(View.VISIBLE);
-        showConfirmName.setText(getConfirmName);
+
+        //check if user entered a name
+        if(getConfirmName.equals("")){
+            //pop up error message
+            Utilities.showAlert(this, "Please enter a name.");
+        } else {
+            //show name
+            TextView showConfirmName = (TextView) findViewById(R.id.name_view);
+            showConfirmName.setVisibility(View.VISIBLE);
+            showConfirmName.setText(getConfirmName);
+        }
     }
 
     public void onContinueNameClick(View view){
