2da94443ec7ccc838cb6c140f19557e608745956
Nachelle Matute <nachellematute@Nachelles-MBP.attlocal.net>
2022-02-12 15:14:57 -0800

    no name entered, click continue button

--- app/src/main/java/com/example/birdsofafeather/UserNameActivity.java
+++ app/src/main/java/com/example/birdsofafeather/UserNameActivity.java
@@ -45,10 +45,16 @@ public class UserNameActivity extends AppCompatActivity{
         TextView InputUserNameView = findViewById(R.id.input_name_textview);
         String getInputUserNameView = InputUserNameView.getText().toString();
 
-        //set extra
-        intent.putExtra("student_name", getInputUserNameView);
-        startActivity(intent);
-        finish();
+        //check if user entered a name
+        if(getInputUserNameView.equals("")){
+            //pop up error message
+            Utilities.showAlert(this, "Please enter a name.");
+        } else {
+            //set extra
+            intent.putExtra("student_name", getInputUserNameView);
+            startActivity(intent);
+            finish();
+        }
     }
 }
 
