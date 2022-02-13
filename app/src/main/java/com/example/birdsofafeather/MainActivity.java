1f037bf1d2e46d771cb08c7166c7af78a7f52a6d
Lyra Urrutia Reyes <murrutia@ucsd.edu>
2022-02-12 16:24:39 -0800

    Google login working, including silent sign-in

--- app/src/main/java/com/example/birdsofafeather/MainActivity.java
+++ app/src/main/java/com/example/birdsofafeather/MainActivity.java
@@ -1,21 +1,95 @@
 package com.example.birdsofafeather;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-import com.example.birdsofafeather.models.db.AppDatabase;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // [START configure_signin]
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestProfile()
+                .build();
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleSignInClient with the options specified by gso, GoogleSigninOptions.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // [END build_client]
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!isUserSignedIn()) {
+            signIn();
+        }
+    }
+
+    // Check for Google Sign In
+    private boolean isUserSignedIn () {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        return account != null;
+    }
+
+    // Google Sign In fuction
+    // Mostly sample code from developers.google.com
+    // https://developers.google.com/android/reference/com/google/android/gms/auth/api/signin/GoogleSignInClient
+    private void signIn() {
+        // Attempt silent sign-in
+        Task<GoogleSignInAccount> task = mGoogleSignInClient.silentSignIn();
+        if (!task.isSuccessful()) {
+            // There's no immediate result ready, waits for the async callback.
+            task.addOnCompleteListener(new OnCompleteListener<GoogleSignInAccount>() {
+                @Override
+                public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                    try {
+                        task.getResult(ApiException.class);
+                    } catch (ApiException apiException) {
+                        // You can get from apiException.getStatusCode() the detailed error code
+                        // e.g. GoogleSignInStatusCodes.SIGN_IN_REQUIRED means user needs to take
+                        // explicit action to finish sign-in;
+                        // Please refer to GoogleSignInStatusCodes Javadoc for details
+                        if (apiException.getStatusCode() == GoogleSignInStatusCodes.SIGN_IN_REQUIRED) {
+                            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                            startActivityForResult(signInIntent, RC_SIGN_IN);
+                        } else {
+                            // The ApiException status code indicates the detailed failure reason.
+                            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                            Log.w(TAG, "signInResult:failed code=" + apiException.getStatusCode());
+                        }
+                    }
+                }
+            });
+        }
+    }
+
 
     public void onLoginClick(View view) {
         Intent intent = new Intent(this, StartStopSearchActivity.class);
--- app/src/main/java/com/example/birdsofafeather/UserNameActivity.java
+++ app/src/main/java/com/example/birdsofafeather/UserNameActivity.java
@@ -1,106 +1,52 @@
 package com.example.birdsofafeather;
 
-import static android.content.ContentValues.TAG;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
 
 public class UserNameActivity extends AppCompatActivity{
-
-    private static final int RC_SIGN_IN = 9001;
-    private GoogleSignInClient mGoogleSignInClient;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_name);
         TextView ConfirmName = (TextView) findViewById(R.id.name_view);
+        String nameString = ConfirmName.getText().toString();
         ConfirmName.setVisibility(View.INVISIBLE);
 
-        // [START configure_signin]
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestProfile()
-                .build();
-        // [END configure_signin]
-
-        // [START build_client]
-        // Build a GoogleSignInClient with the options specified by gso.
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        // [END build_client]
+        // If name has not been set, and user is signed in
+        if (isUserSignedIn() && nameString.isEmpty()) {
+            GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+            updateName(account);
+        }
     }
 
     @Override
     public void onStart() {
         super.onStart();
+    }
 
-        // [START on_start_sign_in]
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
+    // Check for Google Sign In
+    private boolean isUserSignedIn () {
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateName(account);
-        // [END on_start_sign_in]
+        return account != null;
     }
-
-    // [START onActivityResult]
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-    // [END onActivityResult]
-
-    // [START handleSignInResult]
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-            updateName(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateName(null);
-        }
-    }
-    // [END handleSignInResult]
 
     private void updateName(@Nullable GoogleSignInAccount account) {
         TextView confirmName = findViewById(R.id.input_name_textview);
         String getConfirmName = confirmName.getText().toString();
-        if (account != null) {
+
+        if (account != null && getConfirmName.isEmpty()) {
             confirmName.setText(account.getGivenName());
         }
     }
 
-    // [START signIn]
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-    // [END signIn]
 
     @Override
     protected void onDestroy() {
