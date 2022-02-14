cbd6dd9cb18c2ebbba938fb6444410078416f345
Kathy Li <kal005@ucsd.edu>
2022-02-13 22:44:29 -0800

    Fix tests

--- app/src/test/java/com/example/birdsofafeather/ViewProfileActivityTest.java
+++ app/src/test/java/com/example/birdsofafeather/ViewProfileActivityTest.java
@@ -16,6 +16,7 @@ import com.example.birdsofafeather.models.db.AppDatabase;
 import com.example.birdsofafeather.models.db.Course;
 import com.example.birdsofafeather.models.db.Student;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,51 +24,63 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class ViewProfileActivityTest {
+    Student s1;
+    Student s2;
+    Student s3;
+    Course c1;
+    Course c2;
+    Course c3;
 
     @Before
     public void init() {
         // Initializing a database where Bob and Bill share one class, Bob and Mary share no classes
         AppDatabase db = AppDatabase.singleton(ApplicationProvider.getApplicationContext());
-        db.studentWithCoursesDao().insert(new Student("Bob", "bob.com"));
-        db.studentWithCoursesDao().insert(new Student("Bill", "bill.com"));
-        db.studentWithCoursesDao().insert(new Student("Mary", "mary.com"));
-        db.coursesDao().insert(new Course(1, "CSE 20 FA 2021"));
-        db.coursesDao().insert(new Course(2, "CSE 20 FA 2021"));
-        db.coursesDao().insert(new Course(3, "CSE 15L FA 2021"));
+        s1 = new Student("Bob", "bob.com");
+        s2 = new Student("Bill", "bill.com");
+        s3 = new Student("Mary", "mary.com");
+        db.studentWithCoursesDao().insert(s1);
+        db.studentWithCoursesDao().insert(s2);
+        db.studentWithCoursesDao().insert(s3);
+        c1 = new Course(1, "CSE 20 FA 2021");
+        c2 = new Course(2, "CSE 20 FA 2021");
+        c3 = new Course(3, "CSE 15L FA 2021");
+        db.coursesDao().insert(c1);
+        db.coursesDao().insert(c2);
+        db.coursesDao().insert(c3);
+    }
+
+    @After
+    public void cleanup() {
+        AppDatabase db = AppDatabase.singleton(ApplicationProvider.getApplicationContext());
+        db.studentWithCoursesDao().delete(s1);
+        db.studentWithCoursesDao().delete(s2);
+        db.studentWithCoursesDao().delete(s3);
+        db.coursesDao().delete(c1);
+        db.coursesDao().delete(c2);
+        db.coursesDao().delete(c3);
     }
 
     @Test
-    /* Tests that the name and profile profile are loaded from the database properly */
-    public void testProfileLoad(){
-        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), ViewProfileActivity.class);
-        intent.putExtra("classmate_id", 2);  // Bob is looking at Bill's profile
-        try(ActivityScenario<ViewProfileActivity> scenario = ActivityScenario.launch(intent)) {
-
-            // Multiple assertions in one test to avoid launching too many activities
-            scenario.onActivity(activity -> {
-                scenario.moveToState(Lifecycle.State.CREATED);
-                TextView name = activity.findViewById(R.id.name_view);
-                ImageView picture = activity.findViewById(R.id.profile_picture_view);
-                // Test name is not empty
-                assertEquals (name.getText(), "Bill");
-                // Test picture loaded
-                assertEquals (picture.getTag(), "bill.com");
-            });
-        }
-    }
-
-    @Test
-    /* Tests common courses show up on profile page if they exist */
+    /* Tests common courses and other elements show up on profile page if they exist */
     public void testCommonCourses(){
         Intent intent = new Intent(ApplicationProvider.getApplicationContext(), ViewProfileActivity.class);
         intent.putExtra("classmate_id", 2);
         try(ActivityScenario<ViewProfileActivity> scenario = ActivityScenario.launch(intent)) {
             scenario.onActivity(activity -> {
                 scenario.moveToState(Lifecycle.State.CREATED);
+                // Multiple assertions in one test to avoid launching too many activities
+                TextView name = activity.findViewById(R.id.name_view);
+                ImageView picture = activity.findViewById(R.id.profile_picture_view);
+
+                // Test name is not empty
+                assertEquals (name.getText(), "Bill");
+                // Test picture loaded
+                assertEquals (picture.getTag(), "bill.com");
+
                 TextView courses = activity.findViewById(R.id.common_classes_view);
                 // Test course loaded and visibility
-                assertEquals(courses.getVisibility(), View.VISIBLE);
-                assertEquals(courses.getText(), "CSE 20 FA 2021\n");
+                assertEquals(View.VISIBLE, courses.getVisibility());
+                assertEquals("CSE 20 FA 2021\n", courses.getText());
             });
         }
     }
@@ -81,9 +94,10 @@ public class ViewProfileActivityTest {
             scenario.onActivity(activity -> {
                 scenario.moveToState(Lifecycle.State.CREATED);
                 TextView courses = activity.findViewById(R.id.common_classes_view);
-                assertEquals(courses.getVisibility(), View.VISIBLE);
-                assertEquals(courses.getText(), "\n");
+                assertEquals(View.VISIBLE, courses.getVisibility());
+                assertEquals("", courses.getText());
             });
         }
     }
+
 }
