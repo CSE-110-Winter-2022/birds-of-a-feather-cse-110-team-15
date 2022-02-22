9cc8bd9ae8e7232e20d587d249ebc329c9680bb1
Kathy Li <kal005@ucsd.edu>
2022-02-22 00:01:51 -0800

    Update tests

--- app/src/test/java/com/example/birdsofafeather/FavoriteProfileTest.java
+++ app/src/test/java/com/example/birdsofafeather/FavoriteProfileTest.java
@@ -1,9 +1,13 @@
 package com.example.birdsofafeather;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Intent;
+import android.widget.TextView;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.birdsofafeather.models.db.AppDatabase;
 import com.example.birdsofafeather.models.db.Course;
@@ -11,7 +15,9 @@ import com.example.birdsofafeather.models.db.Student;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class FavoriteProfileTest {
 
     @Before
@@ -38,9 +44,11 @@ public class FavoriteProfileTest {
     /* Tests common courses and other elements show up on profile page if they exist */
     public void testFavoriteFromProfile() {
         Intent intent = new Intent(ApplicationProvider.getApplicationContext(), ViewProfileActivity.class);
-        intent.putExtra("classmate_id", 2);
+        intent.putExtra("classmate_id", 3);
         try (ActivityScenario<ViewProfileActivity> scenario = ActivityScenario.launch(intent)) {
             scenario.onActivity(activity -> {
+                TextView name = activity.findViewById(R.id.name_view);
+                assertEquals("Mary", name.getText());
                     }
             );
         }
