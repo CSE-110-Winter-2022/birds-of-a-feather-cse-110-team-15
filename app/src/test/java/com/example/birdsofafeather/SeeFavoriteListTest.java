872cda424f23e842862248eb7331730c81692d68
Chenyu Zhang <chz042@ucsd.edu>
2022-02-26 23:32:50 -0800

    favorite list test complete

--- app/src/test/java/com/example/birdsofafeather/SeeFavoriteListTest.java
+++ app/src/test/java/com/example/birdsofafeather/SeeFavoriteListTest.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.birdsofafeather.models.db.AppDatabase;
 import com.example.birdsofafeather.models.db.Course;
@@ -17,7 +18,9 @@ import com.example.birdsofafeather.models.db.Student;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class SeeFavoriteListTest {
     @Before
     // Initialize the database where Bob is the user, Bill shares 1 class with him, Mary shares 2,
@@ -26,7 +29,7 @@ public class SeeFavoriteListTest {
         AppDatabase.useTestSingleton(ApplicationProvider.getApplicationContext());
         AppDatabase db = AppDatabase.singleton(ApplicationProvider.getApplicationContext());
         db.studentWithCoursesDao().insert(new Student("Bob", "bob.com"));
-        db.studentWithCoursesDao().insert(new Student("Bill", "bill.com"));
+        db.studentWithCoursesDao().insert(new Student("Bill", "bill.com", true));
         db.studentWithCoursesDao().insert(new Student("Mary", "mary.com"));
         db.studentWithCoursesDao().insert(new Student("Toby", "toby.com"));
         // Bob's classes
@@ -52,40 +55,22 @@ public class SeeFavoriteListTest {
     public void testViewingFavoriteList(){
         try(ActivityScenario<FavoriteListActivity> scenario = ActivityScenario.launch(FavoriteListActivity.class)){
             scenario.onActivity(activity -> {
-                RecyclerView studentList = activity.findViewById(R.id.students_recycler_view);
+                RecyclerView studentList = activity.findViewById(R.id.favorites_recycler_view);
                 final int studentCount = studentList.getChildCount();
 
-                // There are 3 other students in database, only two should appear in the view
-                assertEquals(2, studentCount);
+                // There are 3 other students in database, only one should appear in the view
+                assertEquals(1, studentCount);
 
                 // Assert Mary and her information was rendered first because she shares more classes
                 View studentEntry = studentList.getChildAt(0);
-                TextView name = studentEntry.findViewById(R.id.classmate_name_text);
-                ImageView headshot = studentEntry.findViewById(R.id.classmate_imageview);
-                TextView classCount = studentEntry.findViewById(R.id.common_course_count_textview);
-                assertEquals("Mary", name.getText());
-                assertEquals("mary.com", headshot.getTag());
-                assertEquals("2", classCount.getText());
+                TextView name = studentEntry.findViewById(R.id.fav_classmate_name_text);
+                ImageView headshot = studentEntry.findViewById(R.id.fav_classmate_imageview);
 
-                // Don't actually know if this is how the log messages should be printed or if
-                // it needs to be more elaborate
-                out.println("Expected: Mary        Actual: " + name.getText());
-                out.println("Expected: mary.com    Actual: " + headshot.getTag());
-                out.println("Expected: 2           Actual: " + classCount.getText());
-
-
-                // Assert Bill's info was rendered next correctly
-                studentEntry = studentList.getChildAt(1);
-                name = studentEntry.findViewById(R.id.classmate_name_text);
-                headshot = studentEntry.findViewById(R.id.classmate_imageview);
-                classCount = studentEntry.findViewById(R.id.common_course_count_textview);
                 assertEquals("Bill", name.getText());
                 assertEquals("bill.com", headshot.getTag());
-                assertEquals("1", classCount.getText());
 
                 out.println("Expected: Bill        Actual: " + name.getText());
                 out.println("Expected: bill.com    Actual: " + headshot.getTag());
-                out.println("Expected: 1           Actual: " + classCount.getText());
             });
         }
     }
