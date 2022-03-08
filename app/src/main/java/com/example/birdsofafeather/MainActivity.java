a84b02287496fea4e707f5be4b777e7d68821452
Nachelle Matute <nachellematute@Nachelles-MBP.attlocal.net>
2022-03-07 23:57:08 -0800

    uncomment google login functionality in MainActivity

--- app/src/main/java/com/example/birdsofafeather/MainActivity.java
+++ app/src/main/java/com/example/birdsofafeather/MainActivity.java
@@ -26,25 +26,24 @@ public class MainActivity extends AppCompatActivity {
         createProfileButton = (Button) findViewById(R.id.Create_Profile_Button);
         favoriteListButton = (Button) findViewById(R.id.Favorites_Button);
         db = AppDatabase.singleton(this);
-//        setButtonVisibilities();
+        setButtonVisibilities();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-//        if (!loginHandler.isUserSignedIn()) {
-//            loginHandler.signIn();
-//        }
+        if (!loginHandler.isUserSignedIn()) {
+            loginHandler.signIn();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-//        setButtonVisibilities();
+        setButtonVisibilities();
     }
-
 
     public void onLoginClick(View view) {
         Intent intent = new Intent(this, StartStopSearchActivity.class);
